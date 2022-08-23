f3234ccfb97e2a15a600f48103de6af865d4d7eb
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-08-23 00:35:14 -0300

    feat: creating put method

--- index.js
+++ index.js
@@ -3,17 +3,10 @@ const { randomUUID } = require("crypto");
 
 let users = [];
 const server = http.createServer((request, response) => {
-  // Cadastro de usuário
-
-  // GET - Listar
-  // POST - Adicionar
-  // PUT - Atualizar
-  // DELETE - Remover
-
   const METHOD = request.method;
   const URL = request.url;
 
-  if (URL === "/users") {
+  if (URL.startsWith("/users")) {
     if (METHOD === "POST") {
       request.on("data", (data) => {
         const body = JSON.parse(data);
@@ -30,6 +23,34 @@ const server = http.createServer((request, response) => {
 
     if (METHOD === "GET") {
       return response.end(JSON.stringify(users));
+    }
+
+    if (METHOD === "PUT") {
+      const paramsSprit = URL.split("/");
+      const id = paramsSprit[2];
+
+      // Receber as informações que quero alterar do body
+      request
+        .on("data", (data) => {
+          const body = JSON.parse(data);
+
+          // Indetificar qual id do usuário quero alterar
+          const userIndex = users.findIndex((user) => user.id === id);
+
+          // Alterar o usuário (ID permanece)
+          users[userIndex] = {
+            id,
+            ...body,
+          };
+        })
+        .on("end", () => {
+          // Retornar usuário alterado
+          return response.end(
+            JSON.stringify({
+              message: "Usuário alterado com sucesso",
+            })
+          );
+        });
     }
   }
 });
